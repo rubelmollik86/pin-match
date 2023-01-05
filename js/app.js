b8a413092e3066efd999a48aefbb739fadf19ea9
rubelmollik86 <rubelmollik86@gmail.com>
2023-01-05 18:46:05 +0600

    use bubble event

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 function getPin() {
     const pin = Math.round(Math.random() * 10000);
-    const pinString = pin +'';
-    if(pinString.length == 4){
+    const pinString = pin + '';
+    if (pinString.length == 4) {
         return pin;
     }
-    else{
+    else {
         // console.log('get three digit and call agin',pin)
         return getPin();
     }
@@ -14,3 +14,19 @@ function genaratePin() {
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('type-numbers');
+    if (isNaN(number)) {
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else {
+        const priviousNumber = calcInput.value;
+        const newNumber = priviousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
